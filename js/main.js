cdfb0ac32d6b0b9ed351cb8cbd6561b548526a03
Javier Jacobo <173719689+JavierE-J@users.noreply.github.com>
2024-07-01 16:37:44 +0000

    Listing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
     $('#btn').click(function() {
-        window.alert("That's it :)");
+        window.alert("Fun Fact: This was a behind the scenes image of my Senior Pictures.");
     });
 });
